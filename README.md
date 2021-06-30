b8328a449388f2e0b0d3ed08c4f2ca611fca9d3d
Wallet <joseouellet@gmail.com>
2021-06-29 21:42:31 -0400

    link logo
    game
    
    
    readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-![image](https://user-images.githubusercontent.com/23088305/39225217-073f7dc8-4819-11e8-86f8-bcfdbc395d10.png)
+[![image](https://user-images.githubusercontent.com/23088305/39225217-073f7dc8-4819-11e8-86f8-bcfdbc395d10.png)](https://nhlgames.github.io/NHLGames/)
 
-| Service          | Check                                   | Status                                                          |
-| ---------------- | --------------------------------------- | --------------------------------------------------------------- |
-| MLBAM API        | Service Available                       | ![image](https://mlbam-games-status.herokuapp.com/ping/mlbam)   |
-| MLBAM API        | Get today's games _(at least one game)_ | ![image](https://mlbam-games-status.herokuapp.com/status/mlbam) |
-| NHL Games server | Service Available                       | ![image](https://mlbam-games-status.herokuapp.com/ping/us)      |
-| NHL Games server | Get stream _(from April 1st 2021)_      | ![image](https://mlbam-games-status.herokuapp.com/status/us)    |
+| Service          | Check                                         | Status                                                                            |
+| ---------------- | --------------------------------------------- | --------------------------------------------------------------------------------- |
+| MLBAM API        | Service is up                                 | ![image](https://mlbam-games-status.herokuapp.com/mlbam/ping)                     |
+| MLBAM API        | Today's schedule returns games                | ![image](https://mlbam-games-status.herokuapp.com/mlbam/schedule)                 |
+| MLBAM API        | Old schedule returns games _(April 1st 2021)_ | ![image](https://mlbam-games-status.herokuapp.com/mlbam/schedule?date=2021-04-01) |
+| NHL Games server | Service is up                                 | ![image](https://mlbam-games-status.herokuapp.com/us/ping)                        |
+| NHL Games server | Today's games streams are up                  | ![image](https://mlbam-games-status.herokuapp.com/us/game)                        |
+| NHL Games server | Old games treams are up _(April 1st 2021)_    | ![image](https://mlbam-games-status.herokuapp.com/us/game?date=2021-04-01)        |
+
+> Checks for today's games will always result in error if the schedule returns no games.
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const axios = require("axios");
+const get = require("lodash.get");
 
 const app = express();
 const port = 3000;
 
+function getUtcDateToLocale(utcDate) {
+  var d = new Date(utcDate);
+  return d.toLocaleDateString("en-CA", { timeZone: "America/Los_Angeles" });
+}
+
 function getToday() {
   const today = new Date();
-  return [today.getFullYear(), today.getMonth() + 1, today.getDate()].join("-");
+  return getUtcDateToLocale(today);
 }
 
 function success(_, res) {
@@ -17,34 +23,62 @@ function error(_, res) {
   res.sendFile(__dirname + "/svg/circle_red.svg");
 }
 
-const checkUs = async (server) => {
-  try {
-    await axios.get(`http://${server}`);
-  } catch {
-    return false;
-  }
-  return true;
-};
-
 function getServers() {
   return Array.from({ length: 5 })
     .map((_, i) => process.env[`SERVER${i + 1}`])
     .filter(Boolean);
 }
 
+async function checkUs(server) {
+  try {
+    await axios.get(`http://${server}`);
+  } catch {
+    return false;
+  }
+  return true;
+}
+
+async function getGameInfoFromMLBAM(date) {
+  const response = await axios.get(
+    "http://statsapi.web.nhl.com/api/v1/schedule",
+    {
+      params: {
+        startDate: date,
+        endDate: date,
+        expand: "schedule.game.content.media.epg",
+      },
+    }
+  );
+
+  const games = get(response.data, "dates[0].games", []);
+  const firstGame = get(games, "[0]");
+  const gameMedias = get(firstGame, "content.media.epg", []);
+  const gameTvMedias = gameMedias.find((m) => m.title === "NHLTV");
+  const gameFirstStreamId = get(gameTvMedias, "items[0].mediaPlaybackId");
+
+  return {
+    totalGames: games.length,
+    streamId: gameFirstStreamId,
+    gameDate: firstGame.gameDate,
+  };
+}
+
 app.get("/", (_, res) => {
   res.sendStatus(200);
 });
 
-app.get("/status/us", async (req, res) => {
+app.get("/us/game", async (req, res) => {
   const [server] = getServers();
   if (!server) return error(req, res);
 
   try {
-    const response = await axios.get(
-      `http://${server}/getM3U8.php?league=NHL&id=${process.env.TEST_GAME_ID}&cdn=akc&date=${process.env.TEST_GAME_DATE}`
+    const { streamId, gameDate } = await getGameInfoFromMLBAM(
+      req.query.date || getToday()
     );
-    console.log(response.data);
+    const date = getUtcDateToLocale(gameDate);
+    const response = await axios.get(
+      `http://${server}/getM3U8.php?league=NHL&id=${streamId}&cdn=akc&date=${date}`
+    );
     if (typeof response.data !== "string" || !response.data.includes(".m3u8")) {
       throw new Error();
     }
@@ -54,13 +88,13 @@ app.get("/status/us", async (req, res) => {
   success(req, res);
 });
 
-app.get("/ping/us", async (req, res) => {
+app.get("/us/ping", async (req, res) => {
   const servers = getServers();
   const results = await Promise.all(servers.map(checkUs));
   return results.includes(true) ? success(req, res) : error(req, res);
 });
 
-app.get("/ping/mlbam", async (req, res) => {
+app.get("/mlbam/ping", async (req, res) => {
   try {
     const response = await axios.get(
       "http://statsapi.web.nhl.com/api/v1/schedule"
@@ -74,14 +108,12 @@ app.get("/ping/mlbam", async (req, res) => {
   success(req, res);
 });
 
-app.get("/status/mlbam", async (req, res) => {
+app.get("/mlbam/schedule", async (req, res) => {
   try {
-    const response = await axios.get(
-      "http://statsapi.web.nhl.com/api/v1/schedule",
-      { params: { startDate: getToday(), endDate: getToday() } }
+    const { totalGames } = await getGameInfoFromMLBAM(
+      req.query.date || getToday()
     );
-    console.log(response.data);
-    if (response.data.totalGames === 0) {
+    if (totalGames === 0) {
       throw new Error();
     }
   } catch {
